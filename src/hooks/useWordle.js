fda6ee4fece0412048d20bbe8edc4b4fb242898f
Diane <dianeyasminec60@gmail.com>
2022-12-25 11:15:24 -0500

    Update useWordle.js

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -11,7 +11,26 @@ const useWordle = (solution) =>{
 
 // format a guess into an array of letter Objects
 const formatGuess = () =>{
-console.log('formatting the guess -', currentGuess)
+let solutionArray = [...solution]
+let formattedGuess = [...currentGuess].map((l)=> {
+    return {key:l, color:'grey'}
+})
+//find any green letter
+formattedGuess.forEach((l,i)=>{
+   if (solutionArray[i] === l.key){
+    formattedGuess[i].color = 'green'
+    solutionArray[i] = null
+   }
+})
+
+// find any yellow color
+formattedGuess.forEach((l,i)=>{
+    if(solutionArray.includes(l.key) === l.key && l.color !== 'green'){
+        formattedGuess[i].color = 'yellow'
+        solutionArray[solutionArray.indexOf(l.key)] = null
+    }
+})
+return formattedGuess
 }
 // Add new guess to the guesses state
 //update the isCorrect state if the guess is correct 
@@ -42,7 +61,8 @@ if(currentGuess.length !== 5){
 console.log('word must be 5 cas long')
 return
 }
-formatGuess()
+const formatted = formatGuess()
+console.log(formatted)
 
     }
 
