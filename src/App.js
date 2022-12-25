f8dfa0664cb38e7c37b4ffd03d18da38dcaf7bb8
Diane <dianeyasminec60@gmail.com>
2022-12-25 10:10:04 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import Wordle from './components/Wordle'
 
-import './App.css';
+
 
 function App() {
+
+const [solution, setSolution] = useState(null)
+
+
+  useEffect(()=>{
+     fetch('  http://localhost:3001/solutions')
+     .then(response =>{
+      return response.json()
+     })
+     .then(data=> {
+      // console.log(data)
+      // random int between 0 & 14
+      //Math.floor will floor decimal number 4.6 => 4
+      //Math.random get a number etween 0-1
+      const randomSolution = data[Math.floor(Math.random() * data.length)]
+      setSolution(randomSolution.word)
+     })
+  },[setSolution])
+
   return (
     <div className="App">
-     
+     <h1>Wordle</h1>
+     {solution && <Wordle solution={solution}/>}
     </div>
   );
 }
