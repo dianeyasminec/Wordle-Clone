e927021a561d1c3a3bc0d3d13194c5c9dd12cddb
Diane <dianeyasminec60@gmail.com>
2022-12-28 11:41:26 -0500

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,6 +11,13 @@ export default function Modal({isCorrect, turn, solution}) {
                  <p >You found the solution in {turn} guesses :) </p>
              </div>
         )}
+         {!isCorrect && (
+             <div>
+                <h1> Nevermind! </h1>
+                 <p className='solution'>{solution}</p>
+                 <p >Better luck next time  :) </p>
+             </div>
+        )}
     </div>
   )
 }
